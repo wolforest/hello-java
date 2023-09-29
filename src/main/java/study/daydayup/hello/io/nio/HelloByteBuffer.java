9f5e823ddee663bfac7f818b39217746dccbc307
winglechen <winglechen@gmail.com>
2023-09-29 23:16:24 +0800

    feature: add HelloByteBuffer

--- src/main/java/study/daydayup/hello/io/nio/HelloByteBuffer.java
+++ src/main/java/study/daydayup/hello/io/nio/HelloByteBuffer.java
@@ -4,24 +4,61 @@ import java.nio.ByteBuffer;
 import study.daydayup.hello.utils.PrintUtils;
 
 public class HelloByteBuffer {
-
+    /**
+     * things you need to know about buffer
+     * 1. two ways to initialize ByteBuffer
+     *      1.1. ByteBuffer.allocateDirect(capacity)
+     *          -> DirectByteBuffer : memory not in jvm
+     *      1.2. ByteBuffer.allocate(capacity)
+     *          -> HeapByteBuffer : memory in jvm
+     *      1.3 clear the buffer
+     *          -> buffer.clear()
+     * 2. three attributes of ByteBuffer
+     *      2.1 capacity
+     *      2.2 position: current write/read position
+     *      2.3 limit:
+     * 3. put/get and flip
+     *      3.1 flip: switch write/read mode
+     *      3.2 putXXXX /getXXXX
+     *
+     * @param args command line args
+     */
     public static void main(String[] args) {
-        //two ways to initialize ByteBuffer
-        //1. ByteBuffer.allocateDirect() -> DirectByteBuffer
-        //2. ByteBuffer.allocate() -> HeapByteBuffer
+        initByteBuffer();
 
+        showAttributes();
 
-
-        helloDirectBuffer();
-        helloHeapBuffer();
+        readAndWrite();
     }
 
     /**
-     * DirectByteBuffer demo
+     * two ways to initialize ByteBuffer
+     * 1. ByteBuffer.allocateDirect(capacity)
+     *      -> DirectByteBuffer : memory not in jvm
+     * 2. ByteBuffer.allocate(capacity)
+     *      -> HeapByteBuffer : memory in jvm
+     * 3. clear the buffer
+     *      -> buffer.clear()
      */
-    public static void helloDirectBuffer() {
-        PrintUtils.starLine("direct buffer demo");
-        PrintUtils.blankLine(2);
+    private static void initByteBuffer() {
+        // DirectByteBuffer -> memory not in jvm
+        ByteBuffer directByteBuffer = ByteBuffer.allocateDirect(1024 * 2);
+        directByteBuffer.clear();
+
+        // HeapByteBuffer -> memory in jvm
+        ByteBuffer heapByteBuffer = ByteBuffer.allocate(1024 * 2);
+        heapByteBuffer.clear();
+    }
+
+    /**
+     * three attributes of ByteBuffer
+     * 1 capacity
+     * 2 position: current write/read position
+     * 3 limit:
+     *      3.1 equals capacity in write mode
+     *      3.2 max readable length in read mode
+     */
+    public static void showAttributes() {
         ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
 
         PrintUtils.starLine("buffer attribute");
@@ -31,9 +68,11 @@ public class HelloByteBuffer {
     }
 
     /**
-     * HeapByteBuffer demo
+     * read, write and flip
+     * 1. flip: switch write/read mode
+     * 2. putXXXX /getXXXX
      */
-    private static void helloHeapBuffer() {
+    private static void readAndWrite() {
         PrintUtils.starLine("heap buffer demo");
         ByteBuffer buffer = ByteBuffer.allocate(1024);
     }
